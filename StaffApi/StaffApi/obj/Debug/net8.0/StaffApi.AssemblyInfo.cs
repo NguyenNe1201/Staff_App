5ade1ef98b70792de8f667597bacefa6fad59d11
Nguyên Nè <94231858+NguyenNe1201@users.noreply.github.com>
2024-09-11 10:55:07 +0700

    update

--- StaffApi/StaffApi/obj/Debug/net8.0/StaffApi.AssemblyInfo.cs
+++ StaffApi/StaffApi/obj/Debug/net8.0/StaffApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaffApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f16fb45d78bb7d85d6380ed55f81792d46827402")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaffApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaffApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
